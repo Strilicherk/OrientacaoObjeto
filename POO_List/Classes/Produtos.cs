51f368291fe8c68c21eac35809aa77c2e37b9f1f
MatheusStrilicherkDEV <matheuscherk@hotmail.com>
2021-01-08 14:14:07 -0300

    lista

---
+++ POO_List/Classes/Produtos.cs
@@ -0,0 +1,24 @@
+namespace Lista_de_Objetos.Classes
+{
+    public class Produtos
+    {
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+        public float Preco { get; set; }
+        
+        public Produtos(){
+
+        }
+
+        public Produtos(int codigo, string nome, float preco ){
+        
+            this.Codigo = codigo;
+            this.Nome = nome;
+            this.Preco = preco;
+
+
+        }
+        
+        
+    }
+}
---
+++ POO_List/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Lista_de_Objetos.Classes;
+
+namespace Lista_de_Objetos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            List<Produtos> produtos = new List<Produtos>();
+ 
+
+            Produtos[] arrayProdutos = {};
+
+            produtos.Add(new Produtos(1, "banana", 58f));
+            produtos.Add(new Produtos(2, "maça", 26f));
+            produtos.Add(new Produtos(3, "goiaba", 256f));
+            
+            Produtos p1 = new Produtos();
+            p1.Nome = "Abacaxi";
+            p1.Codigo = 4022;
+            p1.Preco = 2;
+
+            produtos.Add(p1);
+
+
+        }
+    }
+}
